af1e5141883a9e628f62486303f0b1695e09b159
eddah73 <eddahsjr@gmail.com>
2024-06-27 00:13:00 +0300

    function inside a function

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity ="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+    // console.log(`This Saturday, I want to ${activity}!`);
+}
+// saturdayFun();
+//function without a name is anaymous function
+const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+};
+//mondayWork();
+function wrapAdjective(flair = '*') {
+	let adj = 'special';
+	function withFlair(adj) {
+		return `You are ${flair}${adj}${flair}!`
+	}
+	return withFlair;
+}
+// wrapAdjective("%")("a dedicated programmer");
